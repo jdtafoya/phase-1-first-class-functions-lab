a385d1fce10e90c4afe4bbbc560f05dc3b08beb0
Joshua Tafoya <tafoyajd.91@gmail.com>
2023-05-22 21:20:40 -0700

    created first class functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array) {
+    return [array[0], array[1]];
+}
+
+const returnLastTwoDrivers = function(array) {
+    return [array[2], array[3]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return function(fare) {
+        return (fare * int);
+    }
+}
+
+const fareDoubler = function(fare) {
+    return fare * 2
+}
+
+const fareTripler = function(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(array, drivers) {
+    return drivers(array);
+}
+
+
